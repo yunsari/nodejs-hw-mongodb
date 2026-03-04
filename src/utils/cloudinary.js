07bbd625ca19081b4e1b651fc51ec1b32e83e8f1
yunsari <sariyunus@proton.me>
2026-03-04 13:31:48 +0300

    cloudaniry

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary';
 import env from './env.js';
 
 cloudinary.config({
-  cloud_name: env('CLOUDINARY_CLOUD_NAME'),
-  api_key: env('CLOUDINARY_API_KEY'),
-  api_secret: env('CLOUDINARY_API_SECRET'),
+  cloud_name: env('CLOUDINARY_NAME'),
+  api_key: env('CLOUDINARY_KEY'),
+  api_secret: env('CLOUDINARY_SECRET'),
 });
 
 export default cloudinary;
